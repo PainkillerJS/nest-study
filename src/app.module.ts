a3812544ddf858b48eac7872ad5e1127bc91ce94
Yuriy <nikorezar@gmail.com>
2022-05-17 23:47:51 +0300

    setting for connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+
 import { AuthModule } from "./auth/auth.module";
 import { ProductModule } from "./product/product.module";
 import { ReviewModule } from "./review/review.module";
 import { TopPageModule } from "./top-page/top-page.module";
 
 @Module({
-  imports: [AuthModule, ProductModule, ReviewModule, TopPageModule],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [AuthModule, ProductModule, ReviewModule, TopPageModule, ConfigModule.forRoot()],
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
