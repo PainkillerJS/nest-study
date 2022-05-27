40ef842a4a28ba851377e1a0b59ce13c085f86ef
Yuriy <nikorezar@gmail.com>
2022-05-27 20:25:47 +0300

    add support static file

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,9 +1,19 @@
 import { Module } from "@nestjs/common";
+import { ServeStaticModule } from "@nestjs/serve-static";
+
+import { resolve } from "path";
+import { path } from "app-root-path";
 
 import { FilesController } from "./files.controller";
 import { FilesService } from "./files.service";
 
 @Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: resolve(path, "upload"),
+      serveRoot: resolve("static")
+    })
+  ],
   controllers: [FilesController],
   providers: [FilesService]
 })
